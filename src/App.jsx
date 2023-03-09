ff1f873200976ce27b54099c4ed4ca0f7cfa14ef
romi <romi.varnier@gmail.com>
2023-03-09 13:56:58 +0100

    app.js update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './pages/Layout/Layout'
+import Home from './pages/Home/Home'
+import Store from './pages/Store/Store'
+import About from './pages/About/About'
+import Contact from './pages/Contact/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+
+        <Route path="/" component={<Home />} /> 
+        <Route path="/store" component={<Store />} />
+        <Route path="/about" component={<About />} />
+        <Route path="/contact" component={<Contact />} />
+        <Route path="/search/:keyword" component={<Store />} />
+        
+        {/* <Route element={<IsLoggedOut />}>
+          <Route path="/login" component={<LoginPage />} />
+          <Route path="/register" component={<RegisterPage />} />
+        </Route> */}
+
+        {/* <Route element={<ProtectedRoute />}>
+          <Route path="/profile" component={<ProfilPage />} />
+          <Route path="/cart/:id" component={<CartPage />} />
+          <Route path="/admin/userlist" component={<UserListPage />} />
+          <Route path="/admin/user/:id/edit" component={<UserEditPage/>} />
+          <Route path="/admin/productlist" component={<ProductListPage />} />
+          <Route path="/admin/product/:id/edit" component={<ProductEditPage />} />
+          <Route path="/admin/orderlist" component={<OrderListPage />} />
+        </Route> */}
+      </Route>
+
+        {/* <Route path="/product/:id" component={ProductPage} />
+        <Route path="/order/:id" component={OrderPage} />
+        <Route path="/placeorder" component={PlaceOrderPage} />
+        <Route path="/shipping" component={ShippingPage} />
+        <Route path="/payment" component={PaymentPage} />
+
+        <Route path="*" element={<Error />} /> */}
+    </Routes>
+  </div>
   )
 }
 
---
+++ src/pages/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+// import { useContext } from 'react'
+// import { AuthContext } from '../../context/AuthContext'
+import React from 'react'
+import './Layout.css'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Layout = () => {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+    </>
+  )
+}
+
+export default Layout
