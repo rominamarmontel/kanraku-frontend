b98e450ca7b7b314f511b2a4b6c505b1457e0828
Marianne Agulhon <marianne.agulhon@gmail.com>
2023-03-13 11:47:29 +0100

    fix problems following conflict resolution

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Layout from './pages/Layout/Layout'
-import Home from './pages/Home/Home'
-import Store from './pages/Store/Store'
-import About from './pages/About/About'
-import Contact from './pages/Contact/Contact'
-import Login from './pages/Login/Login'
-import Signup from './pages/Signup/Signup'
-import ProtectedRoute from './pages/Navigation/ProtectedRoute'
-import IsLoggedOut from './pages/Navigation/IsLoggedOut'
-import ProfilePage from './pages/ProfilePage/ProfilePage'
-import FormEditProfile from './pages/ProfilePage/Forms/FormEditProfile'
-import FormEditAddress from './pages/ProfilePage/Forms/FormEditAddress'
-import FormAddAddress from './pages/ProfilePage/Forms/FormAddAddress'
-import Error from './pages/Error/Error'
-import DetailedProductPage from './pages/DetailedProductPage/DetailedProductPage'
-import CartPage from './pages/CartPage/CartPage'
-import OrderPage from './pages/OrderPage/OrderPage'
-import PlaceOrderPage from './pages/PlaceOrderPage/PlaceOrderPage'
-import ShippingPage from './pages/ShippingPage/ShippingPage'
-import PaymentPage from './pages/PaymentPage/PaymentPage'
-import ProductListPage from './pages/Admin/ProductListPage/ProductListPage'
-import ProductEditPage from './pages/Admin/ProductEditPage/ProductEditPage'
-import ProductCreateForm from './pages/Admin/ProductCreateForm/ProductCreateForm'
-import UserListPage from './pages/Admin/UserListPage/UserListPage'
-import UserEditPage from './pages/Admin/UserEditPage/UserEditPage'
-import OrderListPage from './pages/Admin/OrderListPage/OrderListPage'
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout/Layout";
+import Home from "./pages/Home/Home";
+import Store from "./pages/Store/Store";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import ProtectedRoute from "./pages/Navigation/ProtectedRoute";
+import IsLoggedOut from "./pages/Navigation/IsLoggedOut";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import FormEditProfile from "./pages/ProfilePage/Forms/FormEditProfile";
+import FormEditAddress from "./pages/ProfilePage/Forms/FormEditAddress";
+import FormAddAddress from "./pages/ProfilePage/Forms/FormAddAddress";
+import Error from "./pages/Error/Error";
+import DetailedProductPage from "./pages/DetailedProductPage/DetailedProductPage";
+import CartPage from "./pages/CartPage/CartPage";
+import OrderPage from "./pages/OrderPage/OrderPage";
+import PlaceOrderPage from "./pages/PlaceOrderPage/PlaceOrderPage";
+import ShippingPage from "./pages/ShippingPage/ShippingPage";
+import PaymentPage from "./pages/PaymentPage/PaymentPage";
+// import ProductListPage from './pages/Admin/ProductListPage/ProductListPage'
+// import ProductEditPage from "./pages/Admin/ProductEditPage/ProductEditPage";
+import ProductCreateForm from "./pages/Admin/ProductCreateForm/ProductCreateForm";
+import UserListPage from "./pages/Admin/UserListPage/UserListPage";
+import UserEditPage from "./pages/Admin/UserEditPage/UserEditPage";
+import OrderListPage from "./pages/Admin/OrderListPage/OrderListPage";
 
 function App() {
   return (
@@ -46,15 +45,18 @@ function App() {
 
           {/* <Route element={<ProtectedRoute />}> */}
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/profile/edit-informations" element={<FormEditProfile />} />
+          <Route
+            path="/profile/edit-informations"
+            element={<FormEditProfile />}
+          />
           <Route path="/profile/edit-address" element={<FormEditAddress />} />
           <Route path="/profile/add-address" element={<FormAddAddress />} />
           <Route path="/orders" element={<OrderPage />} />
           <Route path="/placeorder" element={<PlaceOrderPage />} />
           <Route path="/shipping" element={<ShippingPage />} />
           <Route path="/payment" element={<PaymentPage />} />
-          <Route path="/admin/productlist" element={<ProductListPage />} />
-          <Route path="/admin/product/:id/edit" element={<ProductEditPage />} />
+          {/* <Route path="/admin/productlist" element={<ProductListPage />} /> */}
+          {/* <Route path="/admin/product/:id/edit" element={<ProductEditPage />} /> */}
           <Route path="/admin/product/create" element={<ProductCreateForm />} />
           <Route path="/admin/userlist" element={<UserListPage />} />
           <Route path="/admin/user/:id/edit" element={<UserEditPage />} />
@@ -66,7 +68,7 @@ function App() {
         </Route>
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Store/Store.jsx
+++ src/pages/Store/Store.jsx
@@ -1,58 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import ProductCard from '../../components/ProductCard/ProductCard'
-import { useSearchParams, Link } from 'react-router-dom'
-import Search from '../../components/Search/Search';
-import myApi from '../../service/service';
-import { Link } from 'react-router-dom'
-import './Store.css'
-
+import React, { useState, useEffect } from "react";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import { useSearchParams, Link } from "react-router-dom";
+import Search from "../../components/Search/Search";
+import myApi from "../../service/service";
+import "./Store.css";
 
 const Store = () => {
-  const [products, setProducts] = useState(null)
-  const [searchParams, setSearchParams] = useSearchParams()
-  const q = searchParams.get('q')
+  const [products, setProducts] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const q = searchParams.get("q");
   const isAdmin = true;
 
   useEffect(() => {
-    const url = q ? `/products/search?q=${q}` : `/products`
-    myApi.get(url)
+    const url = q ? `/products/search?q=${q}` : `/products`;
+    myApi
+      .get(url)
       .then((res) => setProducts(res.data))
-      .catch((e) => console.error(e))
-  }, [searchParams])
+      .catch((e) => console.error(e));
+  }, [searchParams]);
 
   if (!products) {
-    return <div className="Loading">Loading...</div>
+    return <div className="Loading">Loading...</div>;
   }
 
   const handleInputChange = (event) => {
-    setSearchParams({ q: event.target.value })
+    setSearchParams({ q: event.target.value });
   };
 
   return (
     <>
-      <div className='first'>
-        <div className='direction'>
-          <Link to={`/`} >Home</Link><p> / Store</p>
+      <div className="first">
+        <div className="direction">
+          <Link to={`/`}>Home</Link>
+          <p> / Store</p>
         </div>
       </div>
 
-      <div className='Store'>
-        <div className='container'>
-          <div className='title'>
+      <div className="Store">
+        <div className="container">
+          <div className="title">
             <div>
               <h2>STORE</h2>
               <h5>OUR SELECTION</h5>
             </div>
             <Search handleInputChange={handleInputChange} />
           </div>
-          <div className='productCard'>
+          <div className="productCard">
             {products.map((product) => {
-              return <ProductCard key={product._id} product={product} />
+              return <ProductCard key={product._id} product={product} />;
             })}
           </div>
         </div>
       </div>
     </>
-  )
-}
-export default Store
+  );
+};
+export default Store;
