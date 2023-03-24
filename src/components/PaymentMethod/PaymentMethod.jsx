6917108e0cc582b09cfa3d2d3fe2d2f41e7f2a7c
Sophie Dtr <119942370+So-Mina@users.noreply.github.com>
2023-03-24 13:41:14 +0100

    card form

--- src/components/PaymentMethod/PaymentMethod.jsx
+++ src/components/PaymentMethod/PaymentMethod.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './PaymentMethod.css'
+import CreditCardForm from './CreditCardForm/CreditCardForm'
+import PaypalForm from './PayPalForm/PaypalForm'
 
 const PaymentMethod = () => {
   const [paymentMethod, setPaymentMethod] = useState('')
@@ -34,7 +36,7 @@ const PaymentMethod = () => {
     <div className='PaymentMethod'>
 
     <form onSubmit={handleSubmit}>
-      <h3>2. Choose your payment method</h3>
+      <h3>2. Choose payment method</h3>
 
         <table>
         <tbody>
@@ -42,6 +44,7 @@ const PaymentMethod = () => {
             <td><input type='radio' id='creditCard' name='paymentMethod' value='creditCard' checked={paymentMethod === 'creditCard'} onChange={handlePaymentMethodChange} /></td>
             <td><label htmlFor='creditCard'>Credit Card</label></td>
           </tr>
+
           <tr>
             <td><input type='radio' id='paypal' name='paymentMethod' value='paypal' checked={paymentMethod === 'paypal'} onChange={handlePaymentMethodChange} /></td>
             <td><label htmlFor='paypal'>Paypal</label></td>
@@ -52,6 +55,10 @@ const PaymentMethod = () => {
         {errorMessage && <p className='error'>{errorMessage}</p>}
         <button type='submit'>Continue</button>
       </form>
+
+      {paymentMethod === 'creditCard' && <CreditCardForm />}
+      {paymentMethod === 'paypal' && <PaypalForm />}
+
     </div>
   )
 }
